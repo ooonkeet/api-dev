4aad252cbee0412b77b3aaae612a5dccf80ac3c0
Ankit <ankitspare981@gmail.com>
2025-11-16 15:22:28 +0530

    Listing product handlers and errors fixed in handlers and service.

--- e-commApi/internal/products/handlers.go
+++ e-commApi/internal/products/handlers.go
@@ -1,5 +1,23 @@
 package products
 
+import (
+	// "encoding/json"
+	"net/http"
+
+)
+
 type handler struct{
 	service Service
+}
+
+func NewHandler(service Service) *handler {
+	return &handler{
+		service: service,
+	}
+}
+
+func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
+	// call the service -> ListProduct
+	// return JSON in an HTTP response
+	
 }
--- e-commApi/internal/products/service.go
+++ e-commApi/internal/products/service.go
@@ -1,17 +1,7 @@
 package products
 
-import "net/http"
+
 
 type Service interface {
 }
 
-func NewHandler(service Service) *handler {
-	return &handler{
-		service: service,
-	}
-}
-
-func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
-	// call the service -> ListProduct
-	// return JSON in an HTTP response
-}
