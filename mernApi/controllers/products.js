71fc8d4cb7bd112733f1065111cf5389b8091fa7
Ankit <ankitspare981@gmail.com>
2025-11-18 01:47:05 +0530

    Generic typos fixed

--- mernApi/controllers/products.js
+++ mernApi/controllers/products.js
@@ -22,11 +22,11 @@ const getAllProducts = async(req,res)=>{
         apiData=apiData.select(selectFix)
     }
     let page=Number(req.query.page)||1
-    let limit = Number(req.query.limit)||3
+    let limit = Number(req.query.limit)||10
     let skip = (page-1)*limit
     apiData=apiData.skip(skip).limit(limit)
-    const myData=await apiData
-    res.status(200).json({myData,nbHits: myData.length})
+    const Products=await apiData
+    res.status(200).json({Products,nbHits: Products.length})
 }
 
 const getAllProductsTesting=async(req,res)=>{
