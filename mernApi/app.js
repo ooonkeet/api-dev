02f5f580e7653a754bc1e0edee4addad836c1ce8
Ankit <ankitspare981@gmail.com>
2025-11-17 18:24:30 +0530

    Set up Routes and controllers using Express Routers

--- mernApi/app.js
+++ mernApi/app.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 5000;
+const product_routes=require("./routes/products")
 app.get("/",(req,res)=>{
     res.send("Hi, I am live");
 })
+
+// middleware or to set router
+app.use("/api/products",product_routes)
 
 const start=async()=>{
     try{
