6b545f276c05e86475ec98e76cccb90b7428de67
Ankit <ankitspare981@gmail.com>
2025-11-17 23:45:45 +0530

    Add company filter in api and improve its efficacy

--- mernApi/controllers/products.js
+++ mernApi/controllers/products.js
@@ -1,12 +1,22 @@
 const Product=require("../models/products")
 const getAllProducts = async(req,res)=>{
-    const myData=await Product.find(req.query)
+    const {company}=req.query
+    const queryObject={}
+    if(company){
+        queryObject.company=company
+    }
+    const myData=await Product.find(queryObject)
     res.status(200).json({myData})
 }
 
 const getAllProductsTesting=async(req,res)=>{
-    const myData=await Product.find(req.query)
-    console.log(" ~ file: products.js ~line 8 ~getAllProductsTesting ~req.query",req.query);
+    const {company}=req.query
+    const queryObject={}
+    if(company){
+        queryObject.company=company
+    }
+    const myData=await Product.find(queryObject)
+    // console.log(" ~ file: products.js ~line 8 ~getAllProductsTesting ~req.query",req.query);
     
     res.status(200).json({myData})
 }
