f26e99116db65263875d08764e8d9bc953e5d57b
Ankit <ankitspare981@gmail.com>
2025-11-18 01:44:07 +0530

    Added Pagination in REST API using Node & Mongoose

--- mernApi/controllers/products.js
+++ mernApi/controllers/products.js
@@ -21,8 +21,12 @@ const getAllProducts = async(req,res)=>{
         let selectFix=select.split(",").join(" ")
         apiData=apiData.select(selectFix)
     }
+    let page=Number(req.query.page)||1
+    let limit = Number(req.query.limit)||3
+    let skip = (page-1)*limit
+    apiData=apiData.skip(skip).limit(limit)
     const myData=await apiData
-    res.status(200).json({myData})
+    res.status(200).json({myData,nbHits: myData.length})
 }
 
 const getAllProductsTesting=async(req,res)=>{
