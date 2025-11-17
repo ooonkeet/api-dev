dd51bd2e2c3b56cfe15bf59b727ba2bec9efc375
Ankit <ankitspare981@gmail.com>
2025-11-17 23:31:16 +0530

    Adding filtration and searching functionality with query props

--- mernApi/controllers/products.js
+++ mernApi/controllers/products.js
@@ -1,11 +1,13 @@
 const Product=require("../models/products")
 const getAllProducts = async(req,res)=>{
-    const myData=await Product.find({})
+    const myData=await Product.find(req.query)
     res.status(200).json({myData})
 }
 
 const getAllProductsTesting=async(req,res)=>{
-    const myData=await Product.find({})
+    const myData=await Product.find(req.query)
+    console.log(" ~ file: products.js ~line 8 ~getAllProductsTesting ~req.query",req.query);
+    
     res.status(200).json({myData})
 }
 
