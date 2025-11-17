f041ea6e3e4bcd697907ea4997e5406a8dea9c96
Ankit <ankitspare981@gmail.com>
2025-11-17 23:52:58 +0530

    Add advance search functionality in our rest api

--- mernApi/controllers/products.js
+++ mernApi/controllers/products.js
@@ -1,9 +1,16 @@
 const Product=require("../models/products")
 const getAllProducts = async(req,res)=>{
-    const {company}=req.query
+    const {company,name,featured}=req.query
     const queryObject={}
     if(company){
         queryObject.company=company
+    }
+    if(featured)
+    {
+        queryObject.featured=featured
+    }
+    if(name){
+        queryObject.name={$regex: name, $options: "i"}
     }
     const myData=await Product.find(queryObject)
     res.status(200).json({myData})
@@ -14,6 +21,13 @@ const getAllProductsTesting=async(req,res)=>{
     const queryObject={}
     if(company){
         queryObject.company=company
+    }
+    if(featured)
+    {
+        queryObject.featured=featured
+    }
+    if(name){
+        queryObject.name={$regex: name, $options: "i"}
     }
     const myData=await Product.find(queryObject)
     // console.log(" ~ file: products.js ~line 8 ~getAllProductsTesting ~req.query",req.query);
