bd8bc074d82a578960baa1345a43720f99ca369e
Ankit <ankitspare981@gmail.com>
2025-11-18 18:54:54 +0530

    Manual serializatio

--- DRF/api/views.py
+++ DRF/api/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from students.models import Student
 
 # Create your views here.
 def studentsView(request):
-    students = {
-        'id': 1,
-        'name': 'Ankit',
-        'class': 'Computer Science'
-    }
-    return JsonResponse(students)
+    students = Student.objects.all()
+    students_list=list(students.values())
+    return JsonResponse(students_list, safe=False)
