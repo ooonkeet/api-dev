c5432c12fda2afbc768327492845cbb845b08030
Ankit <ankitspare981@gmail.com>
2025-11-23 01:27:18 +0530

    Custom Filter Employees By Name and ID

--- DRF/employees/filter.py
+++ DRF/employees/filter.py
@@ -3,7 +3,11 @@ from .models import Employee
 
 class EmployeeFilter(django_filters.FilterSet):
     designation = django_filters.CharFilter(field_name='designation',lookup_expr='iexact')
+    emp_name=django_filters.CharFilter(field_name='emp_name',lookup_expr='icontains')
+    id=django_filters.RangeFilter(field_name='id')
 
     class Meta:
         model = Employee
-        fields = ['designation']
+        fields = ['designation','emp_name','id'] 
+        # no emp id would work as it iterates over primary key
+        
